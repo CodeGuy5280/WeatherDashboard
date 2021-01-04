7d31ce305a31eb29c3dc10ab8a3808603ac520cb
Alexander Leal <alex.j.leal@live.com>
2021-01-04 00:33:29 -0700

    Finished Weather Dashboard HW.

---
+++ script.js
@@ -0,0 +1,145 @@
+//Selection of elements by id
+var formInputEl = document.getElementById("userCityInput");
+var buttonEl = document.getElementById("searchBtn");
+var cityEl = document.getElementById("cityList");
+var currentDayEl = document.getElementById("dayWeather");
+var dateEL = document.getElementById("date");
+
+//Search for cities by user input to array
+var searchList = JSON.parse(localStorage.getItem('cities')) || []
+//City search
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var city = formInputEl.value.trim();
+  if (city === "" || city === undefined) {
+    alert("must be a valid entry");
+  };
+//Prevent duplicate city
+  if (searchList.includes(city)) {
+    console.log("city already exsists")
+  } else {
+    searchList.push(city)
+  };
+  $("#userCityInput").val(" ");
+  getDayRepo(city);      
+  getForecastRepo(city);
+  createCityList();
+  $("#dayWeather").empty();
+  $("#forecast .row").empty();
+}
+//Function call
+createCityList();
+//Search button given ablitlity to run function 
+$("#searchBtn").on("click", formSubmitHandler);
+function createCityList() {
+  $("#cityList").empty();
+  for (var i = 0; i < searchList.length; i++) {
+    console.log(searchList[i]);
+    localStorage.setItem("cities", JSON.stringify(searchList));
+    var pastCitiesBtn = $("<li>")
+      .addClass("list-group-item cityBtn")
+      .text(searchList[i]);
+    $("#cityList").append(pastCitiesBtn);
+  }
+}
+//Retrieval of city list
+$("#cityList").on("click", "li", function () {
+  var searchInput = $(this).text();
+  getDayRepo(searchInput);
+  getForecastRepo(searchInput);
+  console.log("testing");
+  $("#dayWeather").empty();
+  $("#forecast .row").empty();
+})
+//1 Day forecast 
+function getDayRepo(city) {
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=9215ad3aaa6f9b3960ceccb94240987d&units=imperial";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var cityData = $("<h1>")
+        .addClass("card-text")
+        .text(data.name + " " + moment().format("L"));
+      $("#dayWeather").append(cityData);
+      var imgCurrDay = $("<img>").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png"
+      );
+      $("#dayWeather").append(imgCurrDay);
+      var tempData = $("<div>")
+        .addClass("card-text")
+        .text("Temperature:" + " " + data.main.temp + " °F");
+      $("#dayWeather").append(tempData);
+      var humidityData = $("<div>")
+        .addClass("card-text")
+        .text("Humidity:" + " " + data.main.humidity + " %");
+      $("#dayWeather").append(humidityData);
+      var windData = $("<div>")
+        .addClass("card-text")
+        .text("Windspeed:" + " " + data.wind.speed + " MPH");
+      $("#dayWeather").append(windData);
+      //UV index based on latitude and longitude
+      var cityLat = data.coord.lat;
+      var cityLon = data.coord.lon
+      var requestUvUrl =
+        "https://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial" + "&exclude=minutely,hourly,alerts" + "&appid=9215ad3aaa6f9b3960ceccb94240987d"
+      fetch(requestUvUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          var uvData = $('<div>').addClass("card-text").text("UV Index: ");
+          //conditional statement to change the class of the uvindex (and color) based off index data value
+          var uvIndex = data.value
+          var uvIndexStatus = $('<span>').text(uvIndex);
+          if (uvIndex <= 2) {
+            uvIndexStatus.addClass("badge badge-success");
+          } else if (uvIndex > 2 && uvIndex <= 5) {
+            uvIndexStatus.addClass("badge badge-warning");
+          } else {
+            uvIndexStatus.addClass("badge badge-danger");
+          }
+          uvData.append(uvIndexStatus)
+          $('#dayWeather').append(uvData)
+        });
+    });
+}
+//Fetch functon to bring up cards display
+function getForecastRepo(city) {
+  var requestForecastUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&appid=9215ad3aaa6f9b3960ceccb94240987d";
+  fetch(requestForecastUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.list.length; i++) {
+        if (data.list[i].dt_txt.indexOf("9:00:00") !== -1) {
+          var fiveDays = $("<div>")
+            .addClass("card-text")
+            .text(new Date("date:" + data.list[i].dt_txt).toLocaleDateString());
+          var column = $("<div>").addClass("col-md-2");
+          var card = $("<div>").addClass("card bg-primary text-white");
+          var tempFiveDay = $("<p>")
+            .addClass("card-text")
+            .text("Temp:" + " " + data.list[i].main.temp_max + "°F");
+          var humidityFiveDay = $("<p>")
+            .addClass("card-text")
+            .text("Humidity:" + " " + data.list[i].main.humidity + "%");
+          var imgFiveDay = $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/" +
+            data.list[i].weather[0].icon +
+            "@2x.png");
+          column.append(card.append(fiveDays, tempFiveDay, humidityFiveDay, imgFiveDay));
+          $("#forecast .row").append(column);
+        }
+      }
+    });
+};
